320344b9e60d28387f0f5f267203bbc14a3b5eb3
nico <suttonnico2@gmail.com>
2017-11-04 10:24:27 -0300

    1.1

--- app/src/main/java/com/example/nico/seguimiento/presencia.java
+++ app/src/main/java/com/example/nico/seguimiento/presencia.java
@@ -1,11 +1,36 @@
 package com.example.nico.seguimiento;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
 public class presencia extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.presencia);
+        CheckBox repeatChkBx = ( CheckBox ) findViewById( R.id.checkBox3 );
+        repeatChkBx.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                if ( !isChecked )
+                {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Padres Notificados";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+
+            }
+        });
     }
 }
+
+
